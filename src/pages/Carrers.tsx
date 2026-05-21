2ea88bb78e75f8f6d70244d7088c4a77dd1baca8
Tigana <m.aziz.hlel@gmail.com>
2026-05-21 13:55:44 +0100

    testin

--- src/pages/Carrers.tsx
+++ src/pages/Carrers.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 // --- Design Tokens ---
-const gold = "#C9A96E";
+const gold = "#FFF";
 const cream = "#F5F0E8";
 const dark = "#0C0A08";
 
